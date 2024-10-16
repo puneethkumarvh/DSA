746543a481b63ef3ff259af6beaebe1bbafd82c6
Puneeth kumar vh <69886724+puneethkumarvh@users.noreply.github.com>
2024-10-16 13:49:26 +0530

    Update deleteKthNodeFromEnd.java to add optimal solution

--- StriverA2Z/LinkedList/mediumProblemsOfLL/deleteKthNodeFromEnd.java
+++ StriverA2Z/LinkedList/mediumProblemsOfLL/deleteKthNodeFromEnd.java
@@ -31,3 +31,30 @@ public class Solution
         return head;
     }
 }
+
+
+//Optimal solution - Time complexity is O(N) 
+//here we first move the fast pointer for (k+1)th node and then we move slow and fast pointer until last node is reached.Now where slow pointer is present,it will ne the node which we require to connect to the next to next node.
+public class Solution
+{
+    public static Node removeKthNode(Node head, int K)
+    {
+        if(head.next==null){
+            return null;
+        }
+        Node fast=head;
+        for(int i=1;i<=K;i++){
+            fast=fast.next;
+        }
+        if(fast==null){
+            return head.next;
+        }
+        Node slow=head;
+        while(fast.next!=null){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        slow.next=slow.next.next;
+        return head;
+    }
+}
