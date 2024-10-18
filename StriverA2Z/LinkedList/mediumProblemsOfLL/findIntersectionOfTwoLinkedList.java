b4a7cc5429bc5d7c67a71d009b4c2eaffed57903
Puneeth kumar vh <69886724+puneethkumarvh@users.noreply.github.com>
2024-10-18 13:54:34 +0530

    Create findIntersectionOfTwoLinkedList.java

---
+++ StriverA2Z/LinkedList/mediumProblemsOfLL/findIntersectionOfTwoLinkedList.java
@@ -0,0 +1,26 @@
+//Question link : https://www.naukri.com/code360/problems/-intersection-of-two-linked-lists_630457?utm_source=youtube&utm_medium=affiliate&utm_campaign=Codestudio_Linkedlistseries&leftPanelTabValue=PROBLEM
+//Brute force and my approach 
+//Time complexity is O(N) and space complexity is O(N)
+//code 
+public class Solution {
+    public static int findIntersection(Node firstHead, Node secondHead) {
+        if(firstHead==null || secondHead==null){
+            return 0;
+        }
+        HashMap<Node,Integer> mpp=new HashMap<>();
+        Node first=firstHead;
+        Node second=secondHead;
+        while(first!=null){
+            mpp.put(first,mpp.getOrDefault(first,0)+1);
+            first=first.next;
+            }
+        while(second!=null){
+            if(mpp.containsKey(second)){
+                return second.data;
+            }
+            mpp.put(second,mpp.getOrDefault(second,0)+1);
+            second=second.next;
+        }
+        return 0;
+    }
+}
