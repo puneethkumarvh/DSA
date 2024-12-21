379d7918f50da0113bba88ada62960d3597029be
Puneeth kumar vh <69886724+puneethkumarvh@users.noreply.github.com>
2024-12-21 22:19:10 +0530

    Update q1475.java

--- leetcode/q1475.java
+++ leetcode/q1475.java
@@ -1,4 +1,5 @@
 //Iterate over an array and find the next element as per given condition.
+//O(2N)  time complexity and constant space complexity.
 class Solution {
     public int[] finalPrices(int[] prices) {
         int n=prices.length;
@@ -11,5 +12,23 @@ class Solution {
             }
         }
         return prices;
+    }
+}
+
+
+//Solution 2 : Runs in O(N) time complexity using monotonous stack and using O(N) space complexity in worst case.
+//Kindly refer this video for monotonous stack concept: https://www.youtube.com/watch?v=ALJJcA1uWmU
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int[] result = prices.clone();
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<prices.length;i++){
+            while(st.isEmpty()==false && prices[st.peek()] >= prices[i] ){
+                result[st.pop()]-=prices[i];
+            }
+            st.add(i);
+        }
+        return result;
+        
     }
 }
