93c8cf66949718ca871660d92415c8de8e3e2e9e
Puneeth kumar vh <69886724+puneethkumarvh@users.noreply.github.com>
2024-12-31 18:29:43 +0530

    Update findMissingNumberInArray.java

--- StriverA2Z/Array/Easy/findMissingNumberInArray.java
+++ StriverA2Z/Array/Easy/findMissingNumberInArray.java
@@ -17,3 +17,33 @@ class Solution {
         
     }
 }
+
+//Optimal solution 1: Using XOR function. 
+//code- Time complexity is O(N) space complexity is O(1).
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor1=0;
+        int xor2=0;
+        for(int i=0;i<nums.length;i++){
+            xor1=xor1^i;
+            xor2=xor2^nums[i];
+        }
+        xor1=xor1^nums.length;
+        return xor1 ^ xor2;
+        
+    }
+}
+
+//optimal solution 2 : Using sum of n natural numbers formula.
+//code . Time complexity is O(N) space complexity is O(1).
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int sum = n*(n+1)/2;
+        int cal_sum=0;
+        for(int i:nums){
+            cal_sum += i;
+        }
+        return sum-cal_sum;  
+    }
+}
